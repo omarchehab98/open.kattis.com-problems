2c6b034197afc4a8b16f88b342a3c088314d5091
Omar Chehab <omarchehab98@gmail.com>
2018-10-07 00:13:50 -0400

    Solve carrots

---
+++ carrots/carrots.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numberOfContestants;
+  cin >> numberOfContestants;
+
+  int numberOfHufflePuffProblems;
+  cin >> numberOfHufflePuffProblems;
+
+  cout << numberOfHufflePuffProblems;
+}
